c985246694b0e29c2b85ee98d4eb9b44e3d8d2c4
sarathsreepadmam <157940075+sarathsreepadmam@users.noreply.github.com>
2024-10-14 12:07:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,42 +8,20 @@ include('admin/includes/dbconnection.php');
 
 <head>
     
-    <!--====== Title ======-->
     <title>Food Management Portal | Home</title>
     
-    <!--====== Slick CSS ======-->
     <link rel="stylesheet" href="assets/css/slick.css">
-        
-    <!--====== Font Awesome CSS ======-->
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-        
-    <!--====== Line Icons CSS ======-->
     <link rel="stylesheet" href="assets/css/LineIcons.css">
-        
-    <!--====== Animate CSS ======-->
     <link rel="stylesheet" href="assets/css/animate.css">
-        
-    <!--====== Magnific Popup CSS ======-->
     <link rel="stylesheet" href="assets/css/magnific-popup.css">
-        
-    <!--====== Bootstrap CSS ======-->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    
-    <!--====== Default CSS ======-->
     <link rel="stylesheet" href="assets/css/default.css">
-    
-    <!--====== Style CSS ======-->
     <link rel="stylesheet" href="assets/css/style.css">
     
 </head>
 
 <body>
-    <!--[if IE]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-  <![endif]-->
-   
-   
-    <!--====== PRELOADER PART START ======-->
 
     <div class="preloader">
         <div class="loader">
@@ -61,10 +39,6 @@ include('admin/includes/dbconnection.php');
             </div>
         </div>
     </div>
-
-    <!--====== PRELOADER PART ENDS ======-->
-    
-    <!--====== HEADER PART START ======-->
     
     <header class="header-area">
         <div class="navbar-area headroom">
@@ -114,8 +88,6 @@ include('admin/includes/dbconnection.php');
         </div> <!-- header hero -->
     </header>
     
- 
-    <!--====== FOOTER PART START ======-->
     
     <footer id="footer" class="footer-area bg_cover" style="background-image: url(assets/images/footer-bg.jpg)">
         <div class="container">
@@ -125,58 +97,24 @@ include('admin/includes/dbconnection.php');
         </div> <!-- container -->
     </footer>
     
-    <!--====== FOOTER PART ENDS ======-->
-    
-    <!--====== BACK TOP TOP PART START ======-->
-
     <a href="#" class="back-to-top"><i class="lni-chevron-up"></i></a>
 
-    <!--====== BACK TOP TOP PART ENDS ======-->  
-
-
-
-
-    <!--====== Jquery js ======-->
     <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
     <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
-    
-    <!--====== Bootstrap js ======-->
     <script src="assets/js/popper.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
-    
-    <!--====== Slick js ======-->
     <script src="assets/js/slick.min.js"></script>
-    
-    <!--====== Isotope js ======-->
     <script src="assets/js/imagesloaded.pkgd.min.js"></script>
     <script src="assets/js/isotope.pkgd.min.js"></script>
-    
-    <!--====== Counter Up js ======-->
     <script src="assets/js/waypoints.min.js"></script>
     <script src="assets/js/jquery.counterup.min.js"></script>
-    
-    <!--====== Circles js ======-->
     <script src="assets/js/circles.min.js"></script>
-    
-    <!--====== Appear js ======-->
     <script src="assets/js/jquery.appear.min.js"></script>
-    
-    <!--====== WOW js ======-->
     <script src="assets/js/wow.min.js"></script>
-    
-    <!--====== Headroom js ======-->
     <script src="assets/js/headroom.min.js"></script>
-    
-    <!--====== Jquery Nav js ======-->
     <script src="assets/js/jquery.nav.js"></script>
-    
-    <!--====== Scroll It js ======-->
     <script src="assets/js/scrollIt.min.js"></script>
-    
-    <!--====== Magnific Popup js ======-->
     <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    
-    <!--====== Main js ======-->
     <script src="assets/js/main.js"></script>
     
 </body>
