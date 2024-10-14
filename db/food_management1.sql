d8321f342f828439e13c59b52d9f9e9d060709ed
sarathsreepadmam <157940075+sarathsreepadmam@users.noreply.github.com>
2024-10-14 12:15:24 +0530

    Update food_management1.sql

--- db/food_management1.sql
+++ db/food_management1.sql
@@ -3,16 +3,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
---
--- Database: `food_management`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbladmin`
---
-
 CREATE TABLE `tbladmin` (
   `ID` int(10) NOT NULL,
   `AdminName` varchar(200) DEFAULT NULL,
@@ -24,18 +14,12 @@ CREATE TABLE `tbladmin` (
   `AdminRegdate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbladmin`
---
 
 INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Address`, `Email`, `Password`, `AdminRegdate`) VALUES
 (1, 'Admin', 'admin', 5689784592, 'H-911, Vihar Soraj Nagar New Delhi-110011', 'admin@gmail.com', 'admin123', '2020-07-09 11:58:35');
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `tbldonors`
---
 
 CREATE TABLE `tbldonors` (
   `ID` int(10) NOT NULL,
@@ -50,18 +34,12 @@ CREATE TABLE `tbldonors` (
   `mdesc` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbldonors`
---
 
 INSERT INTO `tbldonors` (`ID`, `name`, `CreationDate`, `mobile`, `city`, `area`, `address`, `type`, `specialist`, `mdesc`) VALUES
 (3, 'Test2', '2023-03-06 11:06:16', '9942133944', 'Coimbatore', 'Ramnagar', 'ewfr', 'Old Age Home', NULL, 'asd');
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `tbluser`
---
 
 CREATE TABLE `tbluser` (
   `ID` int(10) NOT NULL,
@@ -73,54 +51,31 @@ CREATE TABLE `tbluser` (
   `RegDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbluser`
---
 
 INSERT INTO `tbluser` (`ID`, `name`, `mobile`, `email`, `password`, `type`, `RegDate`) VALUES
 (1, 'Test1', 9942133944, 'test@gmail.com', '123456', 1, '2023-03-06 08:28:07');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `tbladmin`
---
 ALTER TABLE `tbladmin`
   ADD PRIMARY KEY (`ID`);
 
---
--- Indexes for table `tbldonors`
---
+
 ALTER TABLE `tbldonors`
   ADD PRIMARY KEY (`ID`);
 
---
--- Indexes for table `tbluser`
---
+
 ALTER TABLE `tbluser`
   ADD PRIMARY KEY (`ID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `tbladmin`
---
 ALTER TABLE `tbladmin`
   MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `tbldonors`
---
+
 ALTER TABLE `tbldonors`
   MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `tbluser`
---
+
 ALTER TABLE `tbluser`
   MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
